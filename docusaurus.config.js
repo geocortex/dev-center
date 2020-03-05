fe1cc29dba8695d2fae2eec8103d1170e5344764
Noah-Silvera <noah.a.silvera@gmail.com>
2020-03-05 13:13:15 -0800

    Add Prettier

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,43 +1,39 @@
-const title = 'Developer Center' 
+const title = "Developer Center";
 module.exports = {
-  title,
-  tagline: 'The tagline of my site',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
-  favicon: 'img/favicon.png',
-  organizationName: 'geocortex', // Usually your GitHub org/user name.
-  projectName: 'Geocortex Developer Center', // Usually your repo name.
-  themeConfig: {
-    navbar: {
-      title,
-      logo: {
-        alt: 'Geocortex Logo',
-        src: 'img/logo.png',
-      },
-      links: [
-        {to: 'docs/web/overview', label: 'Web', position: 'left'}
-      ],
-    },
-    footer: {
-      style: 'dark',
-      links: [
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-    },
-  },
-  presets: [
-    [
-      '@docusaurus/preset-classic',
-      {
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          editUrl:
-            'https://github.com/geocortex/dev-center/edit/master/website/',
+    title,
+    tagline: "The tagline of my site",
+    url: "https://your-docusaurus-test-site.com",
+    baseUrl: "/",
+    favicon: "img/favicon.png",
+    organizationName: "geocortex", // Usually your GitHub org/user name.
+    projectName: "Geocortex Developer Center", // Usually your repo name.
+    themeConfig: {
+        navbar: {
+            title,
+            logo: {
+                alt: "Geocortex Logo",
+                src: "img/logo.png",
+            },
+            links: [{ to: "docs/web/overview", label: "Web", position: "left" }],
         },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+        footer: {
+            style: "dark",
+            links: [],
+            copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
         },
-      },
+    },
+    presets: [
+        [
+            "@docusaurus/preset-classic",
+            {
+                docs: {
+                    sidebarPath: require.resolve("./sidebars.js"),
+                    editUrl: "https://github.com/geocortex/dev-center/edit/master/website/",
+                },
+                theme: {
+                    customCss: require.resolve("./src/css/custom.css"),
+                },
+            },
+        ],
     ],
-  ],
 };
