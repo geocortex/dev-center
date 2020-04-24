cf12ab447f18b425f6442483fb892c9f94924114
Ian Schmitz <ianschmitz@gmail.com>
2020-04-24 13:15:52 -0700

    Get rid of tables

--- src/components/ViewerMessaging/MessagingDefinition.tsx
+++ src/components/ViewerMessaging/MessagingDefinition.tsx
@@ -15,7 +15,10 @@ export default function MessagingDefinition(props: MessagingDefinitionProps) {
     const definition = schema.definitions[trimmedName];
 
     if (definition.type !== "object") {
-        console.warn("Tried to render definition for non-object type", trimmedName);
+        console.warn(
+            "Tried to render definition for non-object type",
+            trimmedName
+        );
         return null;
     }
 
@@ -29,29 +32,46 @@ export default function MessagingDefinition(props: MessagingDefinitionProps) {
                 </a>
             </h3>
             {definition.description && (
-                <div className="margin-bottom--md">{definition.description}</div>
+                <div className="margin-bottom--md">
+                    {definition.description}
+                </div>
             )}
             <h4>Properties</h4>
-            {(!definition.properties || Object.keys(definition.properties).length === 0) && (
+            {(!definition.properties ||
+                Object.keys(definition.properties).length === 0) && (
                 <em>This object doesn't currently contain any properties.</em>
             )}
-            {definition.properties &&
-                Object.entries(definition.properties).map(([propName, propDef]) => (
-                    <div key={propName} className="margin-bottom--md">
-                        <div className="margin-bottom--sm">
-                            <code>{propName}</code>
-                            {definition.required?.includes(propName) && (
-                                <span className="badge badge--secondary">Required</span>
-                            )}
-                        </div>
-                        <div className="margin-left--md">
-                            <MessagingArgument definition={propDef} schema={schema} />
-                            {propDef.description && (
-                                <div className="margin-top--sm">{propDef.description}</div>
-                            )}
-                        </div>
-                    </div>
-                ))}
+            {definition.properties && (
+                <div className="margin-left--md">
+                    {Object.entries(definition.properties).map(
+                        ([propName, propDef]) => (
+                            <div key={propName} className="margin-bottom--md">
+                                <div className="margin-bottom--sm">
+                                    <code>{propName}</code>
+                                    {definition.required?.includes(
+                                        propName
+                                    ) && (
+                                        <span className="badge badge--secondary">
+                                            Required
+                                        </span>
+                                    )}
+                                </div>
+                                <div className="margin-left--md">
+                                    <MessagingArgument
+                                        definition={propDef}
+                                        schema={schema}
+                                    />
+                                    {propDef.description && (
+                                        <div className="margin-top--sm">
+                                            {propDef.description}
+                                        </div>
+                                    )}
+                                </div>
+                            </div>
+                        )
+                    )}
+                </div>
+            )}
         </div>
     );
 }
--- src/components/ViewerMessaging/MessagingTypeSummary.tsx
+++ src/components/ViewerMessaging/MessagingTypeSummary.tsx
@@ -12,7 +12,10 @@ function commandIsOperation(command: MessageDefinition) {
     return !!command.output;
 }
 
-function filterObj<T extends object>(raw: T, predicate: (key: string, raw: T) => boolean): T {
+function filterObj<T extends object>(
+    raw: T,
+    predicate: (key: string, raw: T) => boolean
+): T {
     return Object.keys(raw)
         .filter((key) => predicate(key, raw))
         .reduce<T>((obj, key) => {
@@ -26,9 +29,14 @@ export default function MessagingTypeSummary(props: MessagingTypeSummaryProps) {
     let items: MessageSchema["commands"] | MessageSchema["events"];
 
     if (type === "command") {
-        items = filterObj(schema.commands, (item, raw) => !commandIsOperation(raw[item]));
+        items = filterObj(
+            schema.commands,
+            (item, raw) => !commandIsOperation(raw[item])
+        );
     } else if (type === "operation") {
-        items = filterObj(schema.commands, (item, raw) => commandIsOperation(raw[item]));
+        items = filterObj(schema.commands, (item, raw) =>
+            commandIsOperation(raw[item])
+        );
     } else {
         items = schema.events;
     }
@@ -40,41 +48,38 @@ export default function MessagingTypeSummary(props: MessagingTypeSummaryProps) {
                 const linkId = getActionOrEventDefinitionLinkId(key, type);
 
                 return (
-                    <table key={key}>
-                        <tbody>
-                            <tr>
-                                {type === "command" && <td>Command</td>}
-                                {type === "event" && <td>Event</td>}
-                                {type === "operation" && <td>Operation</td>}
-                                <td>
-                                    <a id={linkId} href={`#${linkId}`}>
-                                        <strong>{key}</strong>
-                                    </a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Description</td>
-                                <td>{item.description}</td>
-                            </tr>
-                            <tr>
-                                <td>Argument</td>
-                                <td>
-                                    <MessagingArgument definition={item.input} schema={schema} />
-                                </td>
-                            </tr>
-                            {type === "operations" && item.output && (
-                                <tr>
-                                    <td>Result</td>
-                                    <td>
-                                        <MessagingArgument
-                                            definition={item.output}
-                                            schema={schema}
-                                        />
-                                    </td>
-                                </tr>
-                            )}
-                        </tbody>
-                    </table>
+                    <div key={key} className="margin-bottom--lg">
+                        <h3>
+                            <a id={linkId} href={`#${linkId}`}>
+                                {key}
+                            </a>
+                        </h3>
+                        {item.description && (
+                            <div className="margin-bottom--md">
+                                {item.description}
+                            </div>
+                        )}
+                        <div className="margin-bottom--md">
+                            <h4>Argument</h4>
+                            <div className="margin-left--md">
+                                <MessagingArgument
+                                    definition={item.input}
+                                    schema={schema}
+                                />
+                            </div>
+                        </div>
+                        {type === "operation" && item.output && (
+                            <>
+                                <h4>Result</h4>
+                                <div className="margin-left--md">
+                                    <MessagingArgument
+                                        definition={item.output}
+                                        schema={schema}
+                                    />
+                                </div>
+                            </>
+                        )}
+                    </div>
                 );
             })}
         </>
