ed35efc07b7c372f76db06c45575f3b01c4bb8dd
Noah Silvera <noah.a.silvera@gmail.com>
2020-05-27 15:06:55 -0700

    Restructure mobile docs (#47)

--- src/components/DemoVideo/index.jsx
+++ src/components/DemoVideo/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from "./index.module.css";
 
 // TODO: Define rules for presenting videos:
 // - Specified size?
@@ -7,7 +8,7 @@ import React from "react";
 // - Best tool to record and the recording parameters to use (quality, etc)
 export default function DemoVideo({ src }) {
     return (
-        <video autoPlay loop muted>
+        <video autoPlay loop muted className={styles.video}>
             <source src={src} type="video/webm" />
             Your browser does not support the video tag.
         </video>
